344684e0f5fcf942124c375c7ba42f02ebecac9a
diasdiogo11 <a26534@alunos.ipca.pt>
2024-07-04 04:53:04 +0100

    conclude header and beginning footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common'
+import { RouterLink, RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    RouterLink, 
+    RouterOutlet,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  currentYear: number=new Date().getFullYear();
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common'
+import { RouterLink, RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    RouterLink, 
+    RouterOutlet,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
