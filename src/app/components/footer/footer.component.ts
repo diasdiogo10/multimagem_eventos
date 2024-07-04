f3f123d50488f9956e15ea4298f89bd831ab4963
diasdiogo11 <a26534@alunos.ipca.pt>
2024-07-04 20:08:52 +0100

    conclusion footer and created contact-form

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common'
+import { NgOptimizedImage } from '@angular/common';
 import { RouterLink, RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [
-    NgOptimizedImage,
-    RouterLink, 
-    RouterOutlet,
-  ],
+  imports: [NgOptimizedImage, RouterLink, RouterOutlet],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-  currentYear: number=new Date().getFullYear();
+  currentYear: number = new Date().getFullYear();
 }
