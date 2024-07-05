94d809e680b4933876bbec1b62ccd748bd9f6890
diasdiogo11 <a26534@alunos.ipca.pt>
2024-07-05 17:08:41 +0100

    starting vehicles component and change footer

--- src/app/components/vehicles/vehicles.component.ts
+++ src/app/components/vehicles/vehicles.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-vehicles',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NgOptimizedImage],
   templateUrl: './vehicles.component.html',
   styleUrl: './vehicles.component.scss',
 })
