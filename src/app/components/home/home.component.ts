fc2d6ad23d49228b455b8f97919f0aeb78268678
diasdiogo11 <a26534@alunos.ipca.pt>
2024-07-09 23:30:52 +0100

    complete about section on home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { ContactFormComponent } from '../contact-form/contact-form.component';
+import { RouterLink, RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, ContactFormComponent, FooterComponent],
+  imports: [HeaderComponent, ContactFormComponent, FooterComponent, RouterLink, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
